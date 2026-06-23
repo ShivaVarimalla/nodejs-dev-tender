66f7e127f2139100bb4e34e999d56afb93860f36
ShivaVarimalla <varimallashankar@gmail.com>
2026-06-23 21:42:46 +0530

    addded multipe routes

--- src/app.js
+++ src/app.js
@@ -2,26 +2,21 @@ const express = require("express");
 
 const app = express();
 
+//localhost:7777/user?userId=101&name=shiva
+app.get("/user", (req, res, next) => {
+//   res.send("1st User created successfully!");
+  next()
+},
+[(req, res, next) => {
+    // res.send("2nd User created successfully!");
+    next()
+  },
+  (req,res, next)=>{
+    res.send("3rd user created")
+    next()
+  }]
+);
 
-//localhost:7777/user/101/shiva
-app.get("/user/:userId/:name", (req,res)=>{
-    console.log({...req.params});
-    res.send("User created successfully!")
-})
-
-//localhost:7777/user/:userId=101&name=shiva
-app.get("/user", (req, res)=>{
-    console.log({...req.query})
-    res.send("User created successfully!")
-})
-
-
-app.use("/test",(req,res)=>{
-    res.send("Hello people")
-})
-
-
-
-app.listen(7777, ()=>{
-    console.log("Server has started")
-})
+app.listen(7777, () => {
+  console.log("Server has started");
+});
