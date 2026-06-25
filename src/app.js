a76056535ac355fedcfa43bc89ef89b35d6a9110
ShivaVarimalla <varimallashankar@gmail.com>
2026-06-25 22:55:47 +0530

    Revert "Added post call api"
    This reverts commit b657dcb30b10aa1840be44604e8998e8ab626456.

--- src/app.js
+++ src/app.js
@@ -1,36 +1,24 @@
 const express = require("express");
-const connectDB = require("./config/database");
-const User = require("./models/user");
 
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Meghana",
-    lastName: "Varimalla",
-    emailId: "meghanabaitigeri@gmail.com",
-    password: "1234567890",
-    age: "39",
-    gender: "Female",
-  };
 
-  const user = new User(userObj);
-  try {
-    await user.save();
-    res.send("User added successfully");
-  } catch (error) {
-    res.status(400).send("Error saving the data", error.message);
-  }
+app.get("/user", (req, res) => {
+ try {
+   throw new Error;
+ } catch (error) {
+  res.status(500).send("Internal server error")
+ }
+ 
 });
 
-connectDB()
-  .then(() => {
-    console.log("Database connected successfully");
+app.use("/", (err,req,res, next)=>{
+ if(err){
+  res.status(500).send("Something went wrong")
+ }
+})
 
-    app.listen(7777, () => {
-      console.log("Server has started");
-    });
-  })
-  .catch((err) => {
-    console.error("Database not connected:", err.message);
-  });
+
+app.listen(7777, () => {
+  console.log("Server has started");
+});
