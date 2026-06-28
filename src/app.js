5bac25073f0b6e86b8571e49f4470fb897de3a30
ShivaVarimalla <varimallashankar@gmail.com>
2026-06-28 10:34:20 +0530

    Added get api calls to fetch the users data

--- src/app.js
+++ src/app.js
@@ -4,13 +4,40 @@ const User = require("./models/user");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/signup", (req, res) => {
   const user = new User(req.body);
   user.save();
 
-  res.send("Usre data added succesfully")
+  res.send("Usre data added succesfully");
+});
+
+app.get("/user", async (req, res) => {
+  const email = req.query.emailId;
+  try {
+    const user = await User.findOne({ emailId: email });
+    if (!user) {
+      res.status(404).send("user doesn'texist");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.status(500).send("Something went wrong!");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("users doesn'texist");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 connectDB()
@@ -23,6 +50,3 @@ connectDB()
   .catch((error) => {
     console.log("DB connection err", error);
   });
-// app.listen(7777, () => {
-//   console.log("Server has started");
-// });
